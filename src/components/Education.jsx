7f71400476948c07f4d588f632f32083f6943864
Mayron Castillo <12mayron12@gmail.com>
2025-05-15 21:11:26 -0600

    Refactorización de la sección de educación, eliminando el archivo CSS y adaptando el componente para utilizar Tailwind CSS, mejorando la estructura y la responsividad.

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -2,13 +2,13 @@ import './Education.css'
 
 function Education(props) {
     return (
-        <section id='education' className='education'>
-            <h2 className='education-title'>🎓 Educación</h2>
-            <div className="education-container">
-                <div className="education-card">
-                    <h3> {props.title} </h3>
-                    <p> {props.date} </p>
-                    <p> {props.languaje} </p>
+        <section id='education' className='pt-16 px-4 md:px-0 md:pt-12'>
+            <h2 className='text-[2.5rem] py-[1.5rem] md:pt-12'>🎓 Educación</h2>
+            <div className="max-w-[768px] w-full mx-auto flex flex-col items-center text-center gap-6">
+                <div className="w-full flex flex-col items-center bg-[#fafdff] border-2 border-[#bee6f9] rounded-lg p-4 md:p-5 shadow-lg">
+                    <h3 className='my-2 text-base md:text-xl text-[#333]'> {props.title} </h3>
+                    <p className='my-2 text-sm md:text-base text-[#666]'> {props.date} </p>
+                    <p className='my-2 text-sm md:text-base text-[#666]'> {props.languaje} </p>
                 </div>
             </div>
         </section>
