397e6480c1bf758a9d8d420cb9b29fbb35418b37
Mayron Castillo <12mayron12@gmail.com>
2025-06-06 10:45:36 -0600

    Añadi el proyecto e-commerce

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,69 +2,86 @@ import { useState } from "react";
 import emailjs from "@emailjs/browser";
 
 function Contact() {
-    const [formData, setFormData] = useState({ name: "", email: "", message: "" });
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-    };
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        const serviceID = "service_qyif94f";
-        const templateID = "template_gcft5ck";
-        const publicKey = "RPSpZPMFCF0oknFRj";
+    const serviceID = "service_qyif94f";
+    const templateID = "template_gcft5ck";
+    const publicKey = "RPSpZPMFCF0oknFRj";
 
-        emailjs.send(serviceID, templateID, formData, publicKey)
-            .then((response) => {
-                console.log("Correo enviado con éxito!", response.status, response.text);
-                alert("Mensaje enviado!");
-                setFormData({ name: "", email: "", message: "" });
-            })
-            .catch((error) => {
-                console.error("Error al enviar el correo:", error);
-                alert("Hubo un error al enviar el mensaje. Inténtalo de nuevo.");
-            });
-    };
+    emailjs
+      .send(serviceID, templateID, formData, publicKey)
+      .then((response) => {
+        console.log(
+          "Correo enviado con éxito!",
+          response.status,
+          response.text
+        );
+        alert("Mensaje enviado!");
+        setFormData({ name: "", email: "", message: "" });
+      })
+      .catch((error) => {
+        console.error("Error al enviar el correo:", error);
+        alert("Hubo un error al enviar el mensaje. Inténtalo de nuevo.");
+      });
+  };
 
-    return (
-        <section id="contact" className="pt-12 px-4 md:px-0 md:pt-12">
-            <h3 className="text-[2.5rem] py-[1.5rem] md:pt-12">📬 Contáctame</h3>
-            <section className="text-center p-[40px] bg-[#fafdff] border-2 border-[#bee6f9] rounded-lg shadow-lg m-auto max-w-[768px]">
-                <p className="text-[2rem] text-[#333] mb-5">Puedes escribirme:</p>
+  return (
+    <section id="contact" className="pt-12 px-4 md:px-0 md:pt-12">
+      <h3 className="text-[2.5rem] py-[1.5rem] md:pt-12">📬 Contáctame</h3>
+      <section className="text-center p-[40px] bg-[#fafdff] border-2 border-[#bee6f9] rounded-lg shadow-lg m-auto max-w-[768px]">
+        <p className="text-[2rem] text-[#333] mb-5">Puedes escribirme:</p>
 
-                <form onSubmit={handleSubmit} className="flex flex-col gap-3 w-full max-w-[400px] mx-auto px-4">
-                    <input
-                        type="text"
-                        name="name"
-                        placeholder="Tu Nombre"
-                        value={formData.name}
-                        onChange={handleChange}
-                        className="w-full p-3 border-2 border-[#bee6f9] rounded-md outline-none transition-all duration-300 ease-in-out hover:border-[#85d2f4] focus:border-[#85d2f4]"
-                        required
-                    />
-                    <input
-                        type="email"
-                        name="email"
-                        placeholder="Tu Email"
-                        value={formData.email}
-                        onChange={handleChange}
-                        className="w-full p-3 border-2 border-[#bee6f9] rounded-md outline-none transition-all duration-300 ease-in-out hover:border-[#85d2f4] focus:border-[#85d2f4]"
-                        required
-                    />
-                    <textarea
-                        name="message"
-                        placeholder="Tu Mensaje"
-                        value={formData.message}
-                        onChange={handleChange}
-                        className="w-full p-3 border-2 border-[#bee6f9] rounded-md outline-none transition-all duration-300 ease-in-out hover:border-[#85d2f4] focus:border-[#85d2f4] resize-none h-[120px]"
-                        required
-                    />
-                    <button type="submit" className="bg-[#0b5981] text-white p-3 rounded-md transition-all duration-300 ease-in-out hover:bg-[#083a5a]">Enviar</button>
-                </form>
-            </section>
-        </section>
-    );
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-3 w-full max-w-[400px] mx-auto px-4"
+        >
+          <input
+            type="text"
+            name="name"
+            placeholder="Tu Nombre"
+            value={formData.name}
+            onChange={handleChange}
+            className="w-full p-3 border-2 border-[#bee6f9] rounded-md outline-none transition-all duration-300 ease-in-out hover:border-[#85d2f4] focus:border-[#85d2f4]"
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Tu Email"
+            value={formData.email}
+            onChange={handleChange}
+            className="w-full p-3 border-2 border-[#bee6f9] rounded-md outline-none transition-all duration-300 ease-in-out hover:border-[#85d2f4] focus:border-[#85d2f4]"
+            required
+          />
+          <textarea
+            name="message"
+            placeholder="Tu Mensaje"
+            value={formData.message}
+            onChange={handleChange}
+            className="w-full p-3 border-2 border-[#bee6f9] rounded-md outline-none transition-all duration-300 ease-in-out hover:border-[#85d2f4] focus:border-[#85d2f4] resize-none h-[120px]"
+            required
+          />
+          <button
+            type="submit"
+            className="bg-[#0b5981] text-white p-3 rounded-md transition-all duration-300 ease-in-out hover:bg-[#083a5a]"
+          >
+            Enviar
+          </button>
+        </form>
+      </section>
+    </section>
+  );
 }
 
 export default Contact;
--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,16 +1,17 @@
 const techImages = {
-  HTML: '/logos/html5.svg',
-  CSS: '/logos/css.svg',
-  JavaScript: '/logos/javascript.svg',
-  React: '/logos/react.svg',
-  Tailwind: '/logos/tailwind.svg'
+  HTML: "/logos/html5.svg",
+  CSS: "/logos/css.svg",
+  JavaScript: "/logos/javascript.svg",
+  React: "/logos/react.svg",
+  Tailwind: "/logos/tailwind.svg",
 };
 
 const projects = [
   {
     id: 1,
     title: "Portfolio personal",
-    description: "Web en la que puedes poner tus notas, que utiliza una API, para guardarte tus notas y con varias funcionalidades",
+    description:
+      "Web en la que puedes poner tus notas, que utiliza una API, para guardarte tus notas y con varias funcionalidades",
     technologies: ["HTML", "CSS", "JavaScript", "React", "Tailwind"],
     image: "/logos/portfolio.png",
     github: "https://github.com/Mayron-Castillo/portfolio",
@@ -18,6 +19,16 @@ const projects = [
   },
   {
     id: 2,
+    title: "E-commerce de camisetas de futbol",
+    description:
+      "Web de una e-commerce de camisetas de futbol, con varias funcionalidades",
+    technologies: ["React", "Tailwind"],
+    image: "/logos/e-commerce.png",
+    github: "https://github.com/Mayron-Castillo/e-commerce-futbol",
+    live: "https://e-commerce-futbol-mayron.vercel.app/",
+  },
+  {
+    id: 3,
     title: "Calculadora",
     description: "Web de una calculadora simple",
     technologies: ["HTML", "CSS", "JavaScript"],
@@ -26,27 +37,30 @@ const projects = [
     live: "https://calculadora-mayron.netlify.app/",
   },
   {
-    id: 3,
+    id: 4,
     title: "Dragon Ball",
-    description: "Web de personajes de Dragon Ball desde una API, en la que se muestran todos los personajes de la saga de Dragon Ball",
+    description:
+      "Web de personajes de Dragon Ball desde una API, en la que se muestran todos los personajes de la saga de Dragon Ball",
     technologies: ["HTML", "CSS", "JavaScript"],
     image: "/logos/goku.png",
     github: "https://github.com/Mayron-Castillo/Dragon-ball",
     live: "https://dragonball-mayron.netlify.app/",
   },
   {
-    id: 4,
+    id: 5,
     title: "Rick and Morty",
-    description: "App de Rick and Morty, que se hizo utilizando una API, y en esta app, se muestran varios de los personajes de la serie Rick and Morty",
+    description:
+      "App de Rick and Morty, que se hizo utilizando una API, y en esta app, se muestran varios de los personajes de la serie Rick and Morty",
     technologies: ["HTML", "CSS", "JavaScript"],
     image: "/logos/rickandmorty.png",
     github: "https://github.com/Mayron-Castillo/RickandMorty",
     live: "https://rickandmorty-mayron.netlify.app/",
   },
   {
-    id: 5,
+    id: 6,
     title: "App de notas",
-    description: "Web en la que puedes poner tus notas, que utiliza una API, para guardarte tus notas y con varias funcionalidades",
+    description:
+      "Web en la que puedes poner tus notas, que utiliza una API, para guardarte tus notas y con varias funcionalidades",
     technologies: ["HTML", "CSS", "JavaScript", "React"],
     image: "/logos/notes.png",
     github: "https://github.com/Mayron-Castillo/notes-app-react",
@@ -54,40 +68,47 @@ const projects = [
   },
 ];
 
-function ProjectCard({ project }){
+function ProjectCard({ project }) {
   return (
     <div className="flex flex-col md:flex-row overflow-hidden max-w-[768px] h-auto md:h-[350px] bg-white rounded-xl shadow-md transition-transform duration-300 hover:scale-101">
-      <img 
-        src={project.image} 
-        alt={project.title} 
-        className="w-full md:w-[350px] h-auto object-cover rounded-t-lg md:rounded-l-lg md:rounded-tr-none" 
+      <img
+        src={project.image}
+        alt={project.title}
+        className="w-full md:w-[350px] h-auto object-cover rounded-t-lg md:rounded-l-lg md:rounded-tr-none"
       />
-      <div className='flex flex-col justify-around items-start p-6 md:p-4 gap-4 w-full'>
+      <div className="flex flex-col justify-around items-start p-6 md:p-4 gap-4 w-full">
         <h3 className="text-2xl text-[#0b5981]">{project.title}</h3>
         <p className="text-lg text-[#555]">{project.description}</p>
 
         <div className="flex justify-center flex-wrap gap-4">
           {project.technologies.map((tech, index) => (
-            <div key={index} className="flex items-center gap-2 bg-[#f5f5f5] px-[10px] py-[5px] rounded-md">
-              <img src={techImages[tech]} alt={tech} className="w-[24px] h-[24px]" />
+            <div
+              key={index}
+              className="flex items-center gap-2 bg-[#f5f5f5] px-[10px] py-[5px] rounded-md"
+            >
+              <img
+                src={techImages[tech]}
+                alt={tech}
+                className="w-[24px] h-[24px]"
+              />
               <span className="text-l text-[#333] font-bold">{tech}</span>
             </div>
           ))}
         </div>
 
         <div className="flex flex-col md:flex-row gap-4 justify-center w-full md:w-auto">
-          <a 
-            href={project.github} 
-            target="_blank" 
-            rel="noopener noreferrer" 
+          <a
+            href={project.github}
+            target="_blank"
+            rel="noopener noreferrer"
             className="bg-[#0b5981] text-white px-4 py-2 rounded-md list-none text-l transition-all duration-300 hover:bg-[#083a5a] text-center w-full md:w-auto"
           >
             Ver Código
           </a>
-          <a 
-            href={project.live} 
-            target="_blank" 
-            rel="noopener noreferrer" 
+          <a
+            href={project.live}
+            target="_blank"
+            rel="noopener noreferrer"
             className="bg-[#0b5981] text-white px-4 py-2 rounded-md list-none text-l transition-all duration-300 hover:bg-[#083a5a] text-center w-full md:w-auto"
           >
             Ver Demo
@@ -96,7 +117,7 @@ function ProjectCard({ project }){
       </div>
     </div>
   );
-};
+}
 
 function ProjectsSection() {
   return (
@@ -109,6 +130,6 @@ function ProjectsSection() {
       </div>
     </section>
   );
-};
+}
 
-export default ProjectsSection
+export default ProjectsSection;
